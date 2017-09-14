f449823feee5b2b10a79cb22aa471025f9995cb9
Daniel Brahaney <brahaney@users.noreply.github.com>
2017-09-14 12:05:45 -0400

    Update slack-export.sh

--- slack-export.sh
+++ slack-export.sh
@@ -20,6 +20,6 @@ mkdir -p ${SLACKEXPORT}
 
 cd ${TEMP}
 
-curl -o slack-export.py https://gist.githubusercontent.com/brahaney/fa18124e9260410891948f82cfb6d3c5/raw/daf7bef4cf35a26d914247c76ba0f43bb0e8ede0/slack-export.py
+curl -o slack-export.py https://raw.githubusercontent.com/brahaney/slack-export/master/slack-export.py
 
 python slack-export.py ${SLACK_TOKEN} ${DEST}
